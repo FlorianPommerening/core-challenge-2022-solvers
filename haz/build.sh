3795423cae9ae1f2585cb05d1a85072187466a41
Florian Pommerening <florian.pommerening@unibas.ch>
2022-09-27 15:23:26 +0200

    fix haz/longest and summarize

--- haz/build.sh
+++ haz/build.sh
@@ -2,8 +2,8 @@
 
 cd docker
 docker build -t haz-shortest:latest -f Dockerfile.shortest . 
-# docker build -t haz-longest:latest -f Dockerfile.longest . 
+docker build -t haz-longest:latest -f Dockerfile.longest . 
 cd -
 
 singularity build solver_shortest.sif solver_shortest.def
-# singularity build solver_longest.sif solver_longest.def
+singularity build solver_longest.sif solver_longest.def
