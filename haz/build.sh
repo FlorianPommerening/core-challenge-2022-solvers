a02fe12563a340de3c1a3bc9180d7725b65ca3e6
David Speck <davidjakob.speck@gmail.com>
2022-11-21 13:55:41 +0100

    added symk for longest track component container

--- haz/build.sh
+++ haz/build.sh
@@ -3,7 +3,9 @@
 cd docker
 docker build -t haz-shortest:latest -f Dockerfile.shortest . 
 docker build -t haz-longest:latest -f Dockerfile.longest . 
+docker build -t haz-symk:latest -f Dockerfile.symk . 
 cd -
 
 singularity build solver_shortest.sif solver_shortest.def
 singularity build solver_longest.sif solver_longest.def
+singularity build solver_symk.sif solver_symk.def
